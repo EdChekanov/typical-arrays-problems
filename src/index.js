fe7eaeed1f1d4475ab30d72ec935dbb43cae764a
Ed Chekanov <kide.da@gmail.com>
2021-03-11 10:24:59 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  };
+  let min = array[0];
+  let i = 1;
+  while (i < array.length) {
+    if (min > array[i]) {
+      min = array[i];
+      i++;
+    } else {
+      i++;
+    };
+  };
+  return min;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  };
+  let max = array[0];
+  let i = 1;
+  while (i < array.length) {
+    if (max < array[i]) {
+      max = array[i];
+      i++;
+    } else {
+      i++;
+    };
+  };
+  return max;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  };
+  let sum = 0;
+  let i = 0;
+  while (i < array.length) {
+    sum = sum + array[i];
+    i++;
+  };
+  let avg = (sum / array.length);
+  return avg;
+};
